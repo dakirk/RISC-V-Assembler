ecd861207ecff7ebd611040e506965d68bc41287
David Kirk <dsak@bu.edu>
2019-04-24 19:05:44 -0400

    more examples added

---
+++ test.s
@@ -0,0 +1,13 @@
+addi a0,zero,0
+addi a1,zero,1
+addi a2,zero,2
+addi a3,zero,3
+addi a4,zero,4
+addi a5,zero,5
+addi a6,zero,6
+addi a7,zero,7
+
+sub s2,a7,a5
+and s3,a0,a1
+or s4,a0,a1
+xor s5,a0,a1
